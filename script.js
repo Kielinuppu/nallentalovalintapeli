15a4bda1c1c24208069a816f0f04e70e322e8f52
Kielinuppu <kielinuppu@gmail.com>
2025-01-26 15:00:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const finalFeedback = document.getElementById('finalFeedback');
     const scoreText = document.getElementById('scoreText');
     const nextArrow = document.getElementById('nextArrow');
+    const speakerIcon = document.getElementById('speakerIcon');
 
     // Väitteet ja niiden vastaavat kuvat
     const statements = [
@@ -193,4 +194,5 @@ document.addEventListener('DOMContentLoaded', () => {
     trueButton.addEventListener('click', () => checkAnswer(true));
     falseButton.addEventListener('click', () => checkAnswer(false));
     nextArrow.addEventListener('click', nextQuestion);
+    speakerIcon.addEventListener('click', playQuestionAudio);
 });
